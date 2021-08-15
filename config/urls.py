0775117ada61171bcd4961e5a264e5e03fc9d6f4
Taikan Aihara <s1F101801509@iniad.org>
2021-08-16 06:03:27 +0900

    make it better

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from blog import views as blog_views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]+  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
