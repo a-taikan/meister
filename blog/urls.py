2d88cb63ef5f36c1810172b8e7389141dcd4bf34
Taikan Aihara <s1F101801509@iniad.org>
2021-08-15 18:32:13 +0900

    make it better

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 from django.views.generic import base
-from django.conf.urls.static import static
-from django.conf import settings
+
+
 
 urlpatterns = [
     path('', views.showall, name= 'showall'),
     path('<int:article_id>/', views.blog_show)
-]+  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
