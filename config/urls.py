9d14ba5307443fc2ea2edfd2ffae1709692893fe
Taikan Aihara <s1F101801509@iniad.org>
2021-08-16 06:30:09 +0900

    make it better

--- config/urls.py
+++ config/urls.py
@@ -11,3 +11,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]+  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
