9d32021ecd0d30d7a663bae85860914c16dbdb02
Taikan Aihara <s1F101801509@iniad.org>
2021-08-25 11:24:44 +0900

    make it better

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 from django.views.generic import base
-from blog.views import Index
+
 
 
 
 urlpatterns = [
     path('', views.home, name= 'home'),
     path('<int:article_id>/', views.blog_show, name= 'show'),
-    path('post', Index.as_view(), name='post'),
+    path('post', views.FormView.as_view() ,name="post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Article
 
@@ -17,11 +17,38 @@ def post(request):
     return render(request, 'blog/post.html')
 
 
-from django.views.generic.edit import FormView
 
 from . import forms
+from django.views.generic import TemplateView
+
+class FormView(TemplateView):
+
+    #初期変数定義
+    def __init__(self):
+        self.params = {"Message":"情報を入力してください。",
+                       "form":forms.PostForm(),
+                       }
+
+    #GET時の処理を記載
+    def get(self,request):
+        return render(request, "blog/post.html",context=self.params)
+
+    #POST時の処理を記載
+    def post(self,request):
+        if request.method == "POST":
+            self.params["form"] = forms.PostForm(request.POST, request.FILES)
+            
+            #フォーム入力が有効な場合
+            if self.params["form"].is_valid():
+                #入力項目をデータベースに保存
+                self.params["form"].save(commit=True)
+                self.params["Message"] = "入力情報が送信されました。"
+
+        return render(request, "blog/post.html",context=self.params)
 
 
-class Index(FormView):
-    form_class = forms.PostForm
-    template_name = "blog/post.html"
+
+
+
+
+                
